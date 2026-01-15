4622817bc8f1bb4424624612354515da41d7e60c
Lucas Williams <lucasnwilliams2008@gmail.com>
2026-01-15 14:33:46 -0500

    Adds base dialog component
    Introduces a reusable dialog component with basic styling and close functionality.
    Moves IdeaCard component to the root components folder

---
+++ Ideas-Notepad/src/components/common/Dialog.vue
@@ -0,0 +1,43 @@
+<template>
+  <dialog ref="dialog" class="baseDialog">
+    <header>
+      <button @click="closeDialog">close</button>
+    </header>
+    <slot/>
+  </dialog>
+</template>
+
+<script setup lang="ts">
+import {ref, watch} from "vue";
+
+const visible = defineModel<boolean>('visible', {default: false})
+
+const dialog = ref<HTMLDialogElement>()
+
+watch(visible, (newValue) => {
+  return newValue ? openDialog() : closeDialog()
+})
+
+const openDialog = () => {
+  dialog.value?.showModal()
+  visible.value = true
+}
+
+const closeDialog = () => {
+  dialog.value?.close()
+  visible.value = false
+}
+
+</script>
+
+<style scoped>
+.baseDialog {
+  width: 20rem;
+  height: 10rem;
+
+  header {
+    display: flex;
+    justify-content: flex-end;
+  }
+}
+</style>
