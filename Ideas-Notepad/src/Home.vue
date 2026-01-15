e17a218b5294bdbf44effb63000361667fba690f
Lucas Williams <lucasnwilliams2008@gmail.com>
2026-01-15 11:37:14 -0500

    Initializes basic app layout and routing
    Sets up the basic application layout with a home page.
    Configures routing to navigate between different components.

---
+++ Ideas-Notepad/src/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Ideas Notepad</h1>
+  </div>
+</template>
+
+<script setup
+        lang="ts">
+const test = 'test wakatimestamp'
+</script>
+
+<style scoped>
+
+</style>
--- Ideas-Notepad/src/router/index.ts
+++ Ideas-Notepad/src/router/index.ts
@@ -1,8 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+export enum Home {
+  Home = 'Home',
+}
+
+const routes = [
+  {
+    path: '/appLayout',
+    component: () => import('@/AppLayout.vue'),
+    children: [
+      {
+        path: '/',
+        name: Home.Home,
+        component: () => import('@/Home.vue')
+      }
+    ]
+  }
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
 })
 
 export default router
